3fcff9d769cf6cfddb8f3de222ee8bdee207e8a8
Marcin Gryszko <marcin@grysz.com>
2016-02-04 13:57:05 +0100

    Move todo list to README.me

---
+++ README.md
@@ -0,0 +1,20 @@
+## TODO
+
+Time slice events
+* started - one-time
+* in progress - periodic, every n minutes
+* will elapse soon - repeated, warn me: 10, 5, 3, 1 minutes before time elapses
+* has elapsed - one-time
+* was paused - one-time
+* was stopped - one-time
+
+Timed break events
+* is started - one-time
+* in progress - periodic, every n minutes
+* has elapsed - one-time
+
+Actions
+* play sound
+* speak
+* display (notify via Growl)
+* dim screen (only for has elapsed)
--- src/test/java/TimeSliceTest.java
+++ src/test/java/TimeSliceTest.java
@@ -6,20 +6,6 @@ import rx.schedulers.TestScheduler;
 import java.util.concurrent.TimeUnit;
 
 public class TimeSliceTest {
-    // Events:
-    // started
-    // in progress - periodic event
-    // will elapse soon - repeated event
-    //   warn me: 10, 5, 3, 1 minutes before time elapses
-    // has elapsed
-    // was paused
-    // was stopped
-    // timed break
-    // is started
-    // is in progress
-    // has elapsed
-
-    // actions: play sound, speak, display (notify), dim screen
     @Test
     public void elapse_time_slice_no_events() {
         int duration = 10;
